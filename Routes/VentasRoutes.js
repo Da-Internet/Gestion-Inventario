b1211d69c577ea840800d4c44985ad25d854ab2c
Da_Internet <diego-canto@hotmail.com>
2024-11-11 22:51:15 -0600

    Ventas deberia estar terminado asi

--- Routes/VentasRoutes.js
+++ Routes/VentasRoutes.js
@@ -7,7 +7,20 @@ const rutas = express.Router(); // Creamos un objeto especifico para las rutas
 // Con esto conectamos las rutas al controlador especifico
 const ventasController = require('../Controllers/VentasController.js');
 
+// POST: / registrarVentas - Para Registrar una puntuacion
+rutas.post('/registrarVentas', ventasController.registrarVentas)
 
+// GET: /Ventas/:id - Obtener las Ventas segun ID
+rutas.get('/:id_venta', ventasController.obtenerVentaPorID)
+
+// GET: /Ventas/:metodo_pago - Obtener las Ventas segun Metodo de pago
+rutas.get('/:metodo_pago', ventasController.obtenerVentaPorMetodo)
+
+// DELETE: /Ventas/id - Borrar la venta
+rutas.delete('/id_venta', ventasController.borrarVenta)
+
+// UPDATE: /Ventas/id  - Actualizar la venta 
+rutas.put('/id_venta', ventasController.actualizarVenta)
 
 // Exportaremos el objeto para que se use en otros archivos
 module.exports = rutas;
