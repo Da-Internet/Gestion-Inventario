4a0777d6399508915e7f254a11dcbb8a22d0ac56
Da_Internet <diego-canto@hotmail.com>
2024-11-11 22:52:54 -0600

    Falto corregir aqui

--- Routes/VentasRoutes.js
+++ Routes/VentasRoutes.js
@@ -11,16 +11,16 @@ const ventasController = require('../Controllers/VentasController.js');
 rutas.post('/registrarVentas', ventasController.registrarVentas)
 
 // GET: /Ventas/:id - Obtener las Ventas segun ID
-rutas.get('/:id_venta', ventasController.obtenerVentaPorID)
+rutas.get('/:id_venta', ventasController.obtenerVentasPorID)
 
 // GET: /Ventas/:metodo_pago - Obtener las Ventas segun Metodo de pago
-rutas.get('/:metodo_pago', ventasController.obtenerVentaPorMetodo)
+rutas.get('/:metodo_pago', ventasController.obtenerVentasPorMetodo)
 
 // DELETE: /Ventas/id - Borrar la venta
 rutas.delete('/id_venta', ventasController.borrarVenta)
 
 // UPDATE: /Ventas/id  - Actualizar la venta 
-rutas.put('/id_venta', ventasController.actualizarVenta)
+rutas.put('/id_venta', ventasController.actualizarVentas)
 
 // Exportaremos el objeto para que se use en otros archivos
 module.exports = rutas;
